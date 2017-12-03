484bf2d80f77712e6c52150ad886ead0feafc344
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2017-12-04 05:22:13 +0800

    feat: 新增referer与cookie规则

--- app/middleware/proxy.js
+++ app/middleware/proxy.js
@@ -1,17 +1,33 @@
+'use strict';
+
 const httpProxy = require('http-proxy');
 const atob = require('atob');
+const parse = require('url-parse');
+
+const WHITE_LIST = [
+  '/',
+];
 
 module.exports = (options) => {
   return async function proxy(ctx, next) {
-    if (ctx.query.target) {
-      const target = decodeURI(atob(ctx.query.target));
+    const refer = parse(ctx.header.referer, true);
+    const target = ctx.query.target || refer.query.target || ctx.cookies.get('target');
+    if ((ctx.query.target || refer.query.target) || (!WHITE_LIST.includes(ctx.path) && ctx.cookies.get('target'))) {
+      ctx.cookies.set('target', target);
+      const targetURL = decodeURI(atob(target));
       const proxy = httpProxy.createProxyServer({});
       proxy.on('proxyReq', function(proxyReq, req, res, options) {
-        proxyReq.setHeader('refer', target);
+        proxyReq.setHeader('referer', targetURL);
       });
       proxy.web(ctx.req, ctx.res, {
-        target,
+        target: targetURL,
         changeOrigin: true,
+        cookieDomainRewrite: {
+          '*': ctx.hostname,
+        },
+        headers: {
+          referer: targetURL,
+        },
       });
       await new Promise((resolve, reject) => {
         proxy.on('end', function (req, res, proxyRes) {
@@ -22,6 +38,7 @@ module.exports = (options) => {
         });
       });
     } else {
+      ctx.cookies.set('target', null);
       await next();
     }
   };
