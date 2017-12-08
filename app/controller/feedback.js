5b295a894f47e04aaf027544730337647f121656
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2017-12-08 17:39:38 +0800

    feat: logo添加链接跳转

--- app/controller/feedback.js
+++ app/controller/feedback.js
@@ -6,13 +6,16 @@ class FeedbackController extends Controller {
   async index() {
     let name = 'AnyProxy';
     let logo = 'https://gw.alipayobjects.com/zos/rmsportal/vxuJMojdlMCwzDjREKzY.png';
+    let href = 'http://www.anyproxy.cc/';
     if (this.ctx.host.indexOf('proxyit.cc') > -1) {
       name = 'ProxyIt';
       logo = 'https://gw.alipayobjects.com/zos/rmsportal/ElfMNcmrhlPiYPrETnWI.png';
+      href = 'http://www.proxyit.cc/';
     }
     await this.ctx.render('feedback.nj', {
       name,
       logo,
+      href,
       year: new Date().getFullYear(),
     });
   }
--- app/controller/home.js
+++ app/controller/home.js
@@ -6,9 +6,11 @@ class HomeController extends Controller {
   async index() {
     let name = 'AnyProxy';
     let logo = 'https://gw.alipayobjects.com/zos/rmsportal/vxuJMojdlMCwzDjREKzY.png';
+    let href = 'http://www.anyproxy.cc/';
     if (this.ctx.host.indexOf('proxyit.cc') > -1) {
       name = 'ProxyIt';
       logo = 'https://gw.alipayobjects.com/zos/rmsportal/ElfMNcmrhlPiYPrETnWI.png';
+      href = 'http://www.proxyit.cc/';
     }
     await this.ctx.render('index.nj', {
       name,
