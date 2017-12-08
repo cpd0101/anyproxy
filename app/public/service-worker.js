6c1d7e25c876e56dd5b6f87f441ce50d6d68b16e
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2017-12-09 05:16:50 +0800

    fix: 修复serviceWorker使用错误问题

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -3,13 +3,13 @@ this.addEventListener('fetch', function (event) {
   var url = request.url;
   var reg = /^https\:\/\/(www\.)?(anyproxy|proxyit)\.(cc|cn)/
   if (!reg.test(url)) {
-    var targetRequest = new Request(location.origin + '?target=' + btoa(encodeURI(url)) + '&nocookie=true', {
+    var targetRequest = new Request(location.origin + '/proxy?target=' + btoa(encodeURI(url)) + '&nocookie=true', {
       method: request.method,
       headers: request.headers,
       body: request.body
     });
-    return fetch(targetRequest).then(function (res) {
+    event.respondWith(fetch(targetRequest).then(function (res) {
       return res;
-    })
+    }))
   }
 });
