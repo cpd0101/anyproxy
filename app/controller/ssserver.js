f80233b53e0a934e1ad8c24fe1e5f5a9ce4f0c64
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2017-12-13 23:36:17 +0800

    feat: eslint

--- app/controller/ssserver.js
+++ app/controller/ssserver.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Controller = require('egg').Controller;
 const path = require('path');
 const fs = require('fs');
--- app/schedule/change_password.js
+++ app/schedule/change_password.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path');
 const fs = require('fs');
 
@@ -10,9 +12,9 @@ module.exports = {
     const data = {
       server_port: 8686,
       password: Math.random().toString(36).substr(2),
-      method: "aes-256-cfb",
+      method: 'aes-256-cfb',
     };
-    fs.writeFile(path.join(process.cwd(), 'app', 'config', 'ss-config.json'), JSON.stringify(data), (err, data) => {
+    fs.writeFile(path.join(process.cwd(), 'app', 'config', 'ss-config.json'), JSON.stringify(data), err => {
       if (err) {
         ctx.logger.error(err);
         return;
