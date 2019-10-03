70a39cd05052370dd95da49979607e4a987c7a11
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2019-10-03 17:19:00 +0800

    feat: 更新文件

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -4,11 +4,13 @@ this.addEventListener('fetch', function (event) {
   if (!/^http(s)?\:/.test(url)) {
     return;
   }
-  var reg = /^http(s)?\:\/\/([^\/]+\.)?(anyproxy|proxyit|baidu|zhihu|sohu|alipayobjects|bdstatic|gtimg|qq|itc|sohucs|taobao|tmall|tencent|checkdomain)\.(cc|cn|com|net|org|top|de)/i;
+  var reg = /^http(s)?\:\/\/([^\/]+\.)?(anyproxy|proxyit|baidu|zhihu|sohu|alipayobjects|bdstatic|gtimg|qq|itc|sohucs|tencent|checkdomain)\.(cc|cn|com|net|org|top|de)/i;
   if (reg.test(url)) {
     return;
   }
-  var targetURL = location.origin + '/proxy?target=' + btoa(encodeURI(url)) + '&nocookie=true';
+  url = url.replace(/rf=[^&]*/, 'rf=' + encodeURIComponent(location.href));
+  url = url.replace(/dr=[^&]*/, 'dr=');
+  var targetURL = 'https://www.proxyit.top/proxy?target=' + btoa(encodeURI(url)) + '&nocookie=true';
   var initOptions = {
     method: request.method,
     headers: request.headers
@@ -17,12 +19,25 @@ this.addEventListener('fetch', function (event) {
     if (request.referrer.indexOf(location.origin) === 0) {
       initOptions.referrer = request.referrer;
     } else {
-      initOptions.referrer = location.origin + '/proxy?target=' + btoa(encodeURI(request.referrer)) + '&nocookie=true';
+      initOptions.referrer = 'https://www.proxyit.top/proxy?target=' + btoa(encodeURI(request.referrer)) + '&nocookie=true';
     }
   }
   var method = request.method.toUpperCase();
+  var isClickJs = url.indexOf('https://alimama.alicdn.com/tkapi/click.js') === 0;
   if (method === 'GET' || method === 'HEAD') {
-    event.respondWith(fetch(new Request(targetURL, initOptions)));
+    if (isClickJs) {
+      event.respondWith(fetch(new Request(targetURL, initOptions)).then(function (response) {
+        return response.text().then(function (text) {
+          return new Response('(function () { var top = window; ' + text + ' })()', {
+            status: response.status,
+            statusText: response.statusText,
+            headers: response.headers
+          });
+        });
+      }));
+    } else {
+      event.respondWith(fetch(new Request(targetURL, initOptions)));
+    }
   } else {
     event.respondWith(request.arrayBuffer().then(function (body) {
       initOptions.body = body;
